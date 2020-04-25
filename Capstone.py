abbfc352e0f2ec865af3f3de937672f233d29a95
gauravkum27 <64301368+gauravkum27@users.noreply.github.com>
2020-04-25 13:13:03 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[13]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
